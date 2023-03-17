7531864fb53dc0a4ef5e923e7252fd17d7ced0ba
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-17 16:32:34 +0000

    insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/*this program prints alphabets is lower case */
 /**
- * main - Entry point
+ * main -  Entry point
  *
  * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
