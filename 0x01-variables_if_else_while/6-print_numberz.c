005fa658c7edf4be5ff0ea2f2c4368f6e21d5412
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-17 17:07:06 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*this program prints alphabets is lower case */
+/**
+ * main -  Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
