3d9a43b98c811e49bacd78f4b64f418bc94dfe3f
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-21 20:56:10 +0000

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * print_last_digit - Entry point
+ * @n: intput character
+ * Return: Always 0 (success)
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
