38d387590ded7e1c0f851f4d52ce05147e58ca63
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-22 16:57:50 +0000

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,6 +19,8 @@ void times_table(void)
 			if (c < 10)
 			{
 				_putchar(c + '0');
+				if (c == 9)
+					continue;
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
@@ -27,11 +29,11 @@ void times_table(void)
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
+				if (c == 9)
+					continue;
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (b == 9)
-				continue;
 		}
 		_putchar('\n');
 	}
