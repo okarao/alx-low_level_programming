cce1c01f2517e1468673666e403971d7f63a0f95
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-22 20:42:30 +0000

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,42 +4,14 @@
 #include <stdlib.h>
 /**
  * add - Entry point
- * add two integers and return the result
+ * @n: input variable
+ * @m: input add two integers and return the result
  * Return: Always 0 (success)
  */
 int add(int n, int m)
 {
 	int sum;
 
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			c = a * b;
-			if (c < 10)
-			{
-				_putchar(c + '0');
-			}
-			else
-			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-			}
-			if (b == 9)
-				continue;
-			if (c < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
-		}
-		_putchar('\n');
-	}
+	sum = (n + m);
+	return (sum);
 }
