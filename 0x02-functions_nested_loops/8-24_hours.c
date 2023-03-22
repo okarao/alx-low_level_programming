4d57e410fe5d5485812c2c3a5399e4d16a803103
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-22 11:51:14 +0000

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,10 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	a, b, c, d = 0;
+	a = 0;
+       	b = 0;
+	c = 0;
+       	d = 0;
 	while (a <= 2)
 	{
 		while (b <= 9)
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print every minute
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
