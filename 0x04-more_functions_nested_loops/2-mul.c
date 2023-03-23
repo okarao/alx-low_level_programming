4fc72467d3e6cd5cf023f97e4f38bb8e4eca11ec
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-23 16:26:05 +0000

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * mul -  multiply two numbers
+ * @a: input character
+ * @b: input character
+ * Return: return the product of two integers
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = (a * b);
+	return (c);
+}
