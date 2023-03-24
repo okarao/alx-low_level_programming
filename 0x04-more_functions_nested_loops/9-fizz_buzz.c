7aa08a44d7b9eaafbaffde45bd9281b6172db316
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-24 19:43:49 +0000

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 ==0 && a % 5 == 0)
+		if (a % 3 == 0 && a % 5 == 0)
 			printf("FizzBuzz");
 		else if (a % 3 == 0)
 			printf("Fizz");
@@ -20,7 +20,7 @@ int main(void)
 			printf("Buzz");
 		else
 			printf("%d", a);
-			
+
 		if (a != 100)
 			printf(" ");
 	}
