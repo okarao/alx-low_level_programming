efc34a0ddd3dfd3a6736729903843eb2b57f9c13
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-27 22:02:57 +0000

    Half the lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - prints a string followed by a new line
+ * @str: input string
+ * Return: Output a string
+ */
+void puts2(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
