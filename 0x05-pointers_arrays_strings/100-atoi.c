cb640a42f64c6747c7c248df4e5a5b42a310e9ae
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-28 21:24:26 +0000

    Convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,13 +8,24 @@
 int _atoi(char *s)
 {
 	int i, len;
-	int num;
+	int num, sign, con =0;
 
 	len = strlen(s);
 
 	for (i = 0; i < len; i++)
 	{
-		num = num * 10 + (s[i] - '0');
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+			con = 1;
+		}
+		else if (con == 1)
+			break;
+
+		i++;
 	}
+	num *= sign;
 	return num;
 }
