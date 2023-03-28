5d879d5389f95c5c102966ad11a8e4b628faf2f9
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-28 17:37:13 +0000

    Winning is only half of it

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of the string
+ * @str: the input string
+ * Return: The second half of a string or n last characters
+ */
+
+void puts_half(char *str)
+{
+	int len, n, i, j;
+	int len1;
+
+	len = strlen(str);
+	len1 = len / 2;
+	n = (strlen - 1) / 2;
+
+	if (len % 2 == 0)
+	{
+		for (i = len1; i < len; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		i = len1;
+		j = 0;
+		while (j <= n)
+		{
+			_putchar(str[i]);
+			i++;
+			j++;
+		}
+	}
+	_putchar('\n');
+}
