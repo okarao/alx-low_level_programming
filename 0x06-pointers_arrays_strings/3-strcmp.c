2ae9bca5cd9d56f7444be0f8f69446aa87d64b5e
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-30 14:10:02 +0000

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: input string
+ * @s2: input string
+ * Return: 0 is no match
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	while ( (*s1) != '\0' && (*s2) != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		result = 0;
+	}
+	else
+	{
+		result = (*s1 - *s2);
+	}
+	return (result);
+}
