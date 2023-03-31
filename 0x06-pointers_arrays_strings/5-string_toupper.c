c999258b01835a2b7e0868f927a0596f2fbfa688
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-03-31 07:02:29 +0000

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - convert the lowercase characters to upper
+ * @str: the input string
+ * Return: str with all character uppercase
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] >= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
