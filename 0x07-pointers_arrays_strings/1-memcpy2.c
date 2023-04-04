451a2cbbb1c358b8505d8b2623b4c3c84819ab16
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-04 02:56:15 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _memcpy -  copy contents from src to dest memory locations
+ * @dest: the destination memory location
+ * @src: the source memory location
+ * @n: the number of bytes to be copied
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	while (src[i] != '\0' && i <= n)
+	{
+		dest[j] = src[i];
+		j++;
+		i++;
+	}
+	return (dest);
+}
