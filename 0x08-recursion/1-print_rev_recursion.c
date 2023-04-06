eace6742a4869f62017e6a541426e1351f6f225e
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-06 16:23:39 +0000

    print reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -  print reverse string
+ * @s: the input string
+ *
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+	
