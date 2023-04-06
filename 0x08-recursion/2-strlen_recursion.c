b44dddf2aad54a51ce95075b070a4146fcddbf52
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-06 16:53:52 +0000

    print string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print string length
+ * @s: input string
+ *
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = 1;
+	if (*s != '\0')
+	{
+		len += _strlen_recursion(s + 1);
+	}
+	else
+	{
+		return 0;
+	}
+	return (len);
+}
+
