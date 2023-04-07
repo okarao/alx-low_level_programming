93fadc85d8a196620dcdc87e84017dce448f57bd
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-07 08:08:04 +0000

    Square root recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,32 @@
 #include "main.h"
 
 /**
+ * findSqrt - calculate the square root
+ * @int: input number
+ * @int: a guess
+ * Return: the square root
+ */
+int findSqrt(int, int);
+
+/**
  * _sqrt_recursion - calculate the square root of a number
  * @n: the number
  *
  * Return: The square root
  */
-int findSqrt(int, int);
+
 int _sqrt_recursion(int n)
 {
 	return (findSqrt(n, 1));
 }
-	
 /**
  * findSqrt - calculate the square root
  * @n: the number
  * @g: a guess
- * Retun: the square root
+ * Return: the square root
  */
+
+
 int findSqrt(int n, int g)
 {
 	if (g * g == n)
