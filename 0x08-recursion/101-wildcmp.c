9771a3618c742dbf46b7cdfbb14e7c15eaa17acf
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-08 05:21:06 +0000

    wilcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,7 +11,7 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 != '\0' && *s2 == '*')
 	{
 		return (wildcmp(s1, s2 + 1));
-	}	
+	}
 	else if (*s1 == '\0' && *s2 == '\0')
 	{
 		return (1);
