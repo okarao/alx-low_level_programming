5483b684c351af340bfd32bf83f69d53cb3969c7
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-10 21:50:59 +0000

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -  entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Prints the name of the program
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	
+	printf("%s\n", argv[argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+Low level Programing with C
+0. Understanding arguments to main
+1. what is argc and *argv
+2. use the arguments to main to print progragram name
