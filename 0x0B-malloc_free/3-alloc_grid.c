d2aaf3fdf150a9b782f8597b29f3bb0713291925
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-12 21:24:44 +0000

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * alloc_grid - print a two by two array
+ * @width: number of collumns
+ * @height: number of rows
+ *
+ * Return: pointer to array or zero
+ */
+int **alloc_grid(int width, int height)
+{
+	int *grid, i, j;
+	
+	grid = malloc((width * height) * sizeof(int));
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++;)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	return (grid);
+}
