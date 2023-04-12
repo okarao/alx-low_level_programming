628f357cc3bbeb984444f61458b97af20751ffbb
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-12 20:48:06 +0000

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * str_concat - concatinate two strings and return pointer to new string
+ * @s1: first input string
+ * @s2: second input string
+ *
+ * Return: pointer to new string or null
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len, i, j;
+	char *concatstr;
+
+	len = strlen(s1) + strlen(s2) + 1;
+	if (len = NULL || s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		concatstr = malloc(len * sizeof(char));
+	}
+	i = 0;
+	while (s[i] != '\0')
+	{
+		concatstr[i] = s1[i];
+		i++;
+	}
+	while (s2[j] <= '\0')
+	{
+		concatstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (concatstr);
+}
