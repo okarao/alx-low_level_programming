da5272505836f1179d2a4c6d6d217c0ab8e37cbf
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-13 19:19:10 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,27 +10,26 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int s1_len, s2_len, i, j;
+	unsigned int s1len, s2len, len, i, j;
 	char *con_str;
 
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
-	con_str = malloc((s1_len + s2_len + 1) * sizeof(char));
+	s1len = strlen(s1);
+	s2len = strlen(s2);
+	len = s1len + s2len;
+	con_str = malloc((len + 1) * sizeof(char));
 	if (con_str == NULL)
 	{
 		return (NULL);
 	}
-	else
+	for (i = 0; i < s1len; i++)
 	{
-		for (i = 0; i < strlen(s1); i++)
-		{
-			con_str[i] = s1[i];
-		}
-		for (j = 0; j < strlen(s2); j++)
-		{
-			con_str[s1_len + j] = s2[j];
-		}
-		con_str[s1_len + j] = '\0';
+		con_str[i] = s1[i];
 	}
+	for (j = 0; j < s2len; j++, i++)
+	{
+		con_str[i] = s2[j];
+	}
+	con_str[i] = '\0';
+
 	return (con_str);
 }
