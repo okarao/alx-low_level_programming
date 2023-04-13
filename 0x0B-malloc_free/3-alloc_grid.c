d7ddfd271a84055b80db7b065eb145057fee3904
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-13 19:58:08 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(height * sizeof(int));
-		if (grid[i] =NULL)
+		if (grid[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
 			{
