d6cea23a64761748a96ed14806c5fa2ab630fa6e
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-14 18:33:51 +0000

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	arr = (int *)malloc(nmemb * size);
+	arr = malloc(nmemb * size);
 	if (arr == NULL)
 	{
 		return (NULL);
