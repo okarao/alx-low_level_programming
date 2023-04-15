b4982475fb8c5a44fe1627ec7b66602a7c76a0ac
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-15 19:21:20 +0000

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,6 +2,37 @@
 #include <stdio.h>
 
 /**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i = 0, num = 0, sign = 1;
+
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+	if (s[i] == '-' || s[i] == '+')
+	{
+		sign = 1 - 2 * (s[i++] == '-');
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (num > INT_MAX / 10 || (num == INT_MAX / 10 && s[i] - '0' > 7))
+		{
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
+		}
+		num = 10 * num + (s[i++] - '0');
+	}
+	return (sign * num);
+}
+/**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
@@ -21,11 +52,9 @@ int main(int argc, char *argv[])
 	a = _atoi(argv[1]);
 	b = _atoi(argv[2]);
 
-	printf("num1 is: %d and num2 is: %d\n", a, b);
 	result = a * b;
 
-	printf("%d", result);
-	_putchar('\n');
+	printf("%d\n", result);
 
 	return (0);
 }
