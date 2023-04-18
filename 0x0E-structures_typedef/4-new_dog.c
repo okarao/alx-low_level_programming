5e998cb1ef106caf8fb582167d0c6aea181fdbfa
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-18 20:30:37 +0000

    creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * new_dog - creates a new dog object
+ * @name: dog name array
+ * @age: age variable
+ * @owner: dog owner array
+ * Return: a structure of dog type
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog;
+
+	newdog = malloc(sizeof(dog_t));
+	if (newdog == NULL)
+		return (NULL);
+	newdog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (newdog->name == NULL)
+	{
+		free(newdog);
+		return (NULL);
+	}
+	newdog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (newdog->owner == NULL)
+	{
+		free(newdog);
+		return (NULL);
+	}
+	strcpy(newdog->name, name);
+	newdog->age = age;
+	strcpy(newdog->owner, owner);
+
+	return (newdog);
+}
