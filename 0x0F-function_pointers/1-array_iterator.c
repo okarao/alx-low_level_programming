4d8922696db97fbdd7a9d77bf3f5fc07aef015a9
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-19 12:25:29 +0000

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iteration - calls back a function to iterate an array
+ * @array: input array
+ * @size: the size of array
+ * @action: called back function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+	
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
