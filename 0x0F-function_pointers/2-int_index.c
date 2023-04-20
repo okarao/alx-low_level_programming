4fcc4ed4e5d540ac73b87c70d4293156a60543f3
FREDRICK OGUNDE <okaraogunde@gmail.com>
2023-04-20 09:47:31 +0000

    search for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-	if (cmp == NULL)
-		return;
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
