149251bfa094b32cd443899720ab4a73cd503a3e
Fredrick Okara <okaraogunde@gmail.com>
2023-04-27 22:14:29 +0000

    Add a node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,18 +11,20 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp;
 	unsigned int len = 0;
-	
+
 	while (str[len])
 		len++;
-	
+
 	temp = malloc(sizeof(list_t));
+
 	if (temp == NULL)
-		 return (NULL);
+		return (NULL);
+
 	temp->str = strdup(str);
 	temp->len = len;
 	temp->next = (*head);
 	(*head) = temp;
-	
+
 	return (*head);
 }
 
