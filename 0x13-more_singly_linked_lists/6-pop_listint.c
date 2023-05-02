7ce4c24401f030c2c068cb04298b8a09dd6aae36
Fredrick Okara <okaraogunde@gmail.com>
2023-05-02 17:11:32 +0000

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node
+ * @head: pointer to listint_t
+ *
+ * Return: data n or 0
+ */
+
+int pop_listint(listint_t **head)
+{
+	int result;
+	listint_t *nextnode, *headnode = *head;
+
+	while (head != NULL)
+	{
+		nextnode = headnode->next;
+		free(headnode);
+
+		head = nextnode;
+	}
+	head = NULL;
+}
+
