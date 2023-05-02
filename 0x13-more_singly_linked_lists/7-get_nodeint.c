dfdf5a9999f84da1ddcafc25671de0c292d2a634
Fredrick Okara <okaraogunde@gmail.com>
2023-05-02 20:02:39 +0000

    get node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_nodeint - get a node at an index
+ * @head: list pointer
+ * @index: the index of the node
+ *
+ * Return: node or null
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	if (index == 0)
+		return (head);
+
+	for (i = 0; i < index-1 && head != NULL; i++)
+	{
+		temp = head->next;
+	}
+	temp = head->next;
+
+	return (temp);
+}
