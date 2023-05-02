69524b516f707d5d70e19e21afe09baa14481fb1
Fredrick Okara <okaraogunde@gmail.com>
2023-05-02 19:35:06 +0000

    correct pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -15,10 +15,10 @@ int pop_listint(listint_t **head)
 	if (node == NULL)
 		return (0);
 
-	head = node->next;
+	*head = node->next;
 	free(node);
 
-	result = head->n;
+	result = (*head)->n;
 
 	return (result);
 }
