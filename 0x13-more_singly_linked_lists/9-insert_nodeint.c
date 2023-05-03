69f24e5a99562227a4c38dce775b5aac10581cfe
Fredrick Okara <okaraogunde@gmail.com>
2023-05-03 11:47:40 +0000

    insert a node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
-		for (i = 0; i < idx && temp != NULL; i++)
+		for (i = 0; i < idx -1 && temp != NULL; i++)
 		{
 			temp = temp->next;
 		}
