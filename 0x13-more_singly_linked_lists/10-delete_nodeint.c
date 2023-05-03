3cddfa4ff94dbd7107d3a8f3b2ad789901721786
Fredrick Okara <okaraogunde@gmail.com>
2023-05-03 11:55:19 +0000

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		temp = (*head)->next;
+		temp = *head->next;
 		free(temp);
 		return (1);
 	}
