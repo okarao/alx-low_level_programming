98990b9bcdb5268ad26cd76b13ec1e29910af95b
Fredrick Okara <okaraogunde@gmail.com>
2023-05-03 13:39:12 +0000

    Correction on 10-delete nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,24 +10,28 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *node;
-	listint_t *temp = *head;
+	listint_t *temp;
 
 	unsigned int count = 0;
 
-	if (temp == NULL)
+	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
-		*head = temp->next;
+		*head = (*head)->next;
+		temp = *head;
 		free(temp);
 		return (1);
 	}
+	temp = *head;
 
-	while (count < index && temp->next != NULL)
+	while (count < index - 1 && temp->next != NULL)
 	{
 		temp = temp->next;
 		count++;
 	}
+	if (temp == NULL)
+		return (-1);
 
 	node = temp->next;
 	temp->next = node->next;
