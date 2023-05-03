d6f066a3d1ea14954415790a73d1e24b24e31c17
Fredrick Okara <okaraogunde@gmail.com>
2023-05-03 13:13:32 +0000

    7-get_nodeint corrected

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	temp = head;
 	for (i = 0; i < index && temp != NULL; i++)
 		temp = temp->next;
-	
+
 	if (i == index && temp != NULL)
 		return (temp);
 	else
