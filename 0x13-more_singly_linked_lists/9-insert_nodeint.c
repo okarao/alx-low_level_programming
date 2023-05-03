465e13474f5d1427bd67780f4fd52f75b331c174
Fredrick Okara <okaraogunde@gmail.com>
2023-05-03 19:19:35 +0000

    correction on 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = node;
 		return (node);
 	}
-	for (i = 0; i < idx -1 && temp != NULL; i++)
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
 	{
 		temp = temp->next;
 	}
