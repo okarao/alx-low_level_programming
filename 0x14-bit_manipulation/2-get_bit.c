75ae6f58f46a7fd7845680e87863c50fbd2865e9
Fredrick Okara <okaraogunde@gmail.com>
2023-05-04 20:21:48 +0000

    git the value of bit at an index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - returns a the value of a bit at an index
+ * @n: integer input
+ * @index: the index from which to return bit
+ *
+ * Return: index or -1
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= n)
+		return (-1);
+
+	return ((n >> index) & 1);
+
+}
