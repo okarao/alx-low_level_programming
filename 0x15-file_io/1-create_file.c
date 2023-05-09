7bcf5824286abd143057b1c985c6ebfcba93c722
Fredrick Okara <okaraogunde@gmail.com>
2023-05-09 18:02:02 +0000

    Create a file with content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,12 +9,11 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *myFile;
-	int len = 0, status;
+	int len = 0, status, myFile;
 
 	if (filename == NULL)
 		return (-1);
-	myFile = fopen(filename, O_WRONLY | O_CREAT | O_TRUNC, "0600");
+	myFile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, "0600");
 
 	if (myFile == NULL)
 		return (-1);
@@ -25,10 +24,10 @@ int create_file(const char *filename, char *text_content)
 		status = write(myFile, text_content, len);
 		if (status == -1)
 		{
-			fclose(myFile);
+			close(myFile);
 			return (-1);
 		}
 	}
-	fclose(myFile);
+	close(myFile);
 	return (1);
 }
