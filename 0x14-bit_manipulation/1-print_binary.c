e72ffd77858af334b24dc8ede60e44cfff8f555a
Fredrick Okara <okaraogunde@gmail.com>
2023-05-15 15:33:35 +0000

    correction on print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,19 +9,25 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int binary = 1;
-	int count = 0, i;
+	unsigned long int binary = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int count = 0;
 
-	while ((n >> count) > 0)
-		count++;
 	if (n == 0)
-		_putchar('0');
-
-	for (i = count - 1; i >= 0; i--)
 	{
-		if ((n & (binary << i)) == 0)
-			_putchar('0');
-		else
+		_putchar('0');
+		return;
+	}
+
+	while (binary)
+	{
+		if (n & binary)
+		{
 			_putchar('1');
+			count = 1;
+		}
+		else if (count)
+			_putchar('0');
+
+		binary >>= 1;
 	}
 }
