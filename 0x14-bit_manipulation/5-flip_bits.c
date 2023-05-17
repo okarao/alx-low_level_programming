08321bdf899ac376c337c468dd2a974485c92a64
Fredrick Okara <okaraogunde@gmail.com>
2023-05-17 18:48:37 +0000

    count the number of bits to be flopped to get from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * count_set_bits - counts the the number of set bits
+ * @num: number to be set
+ *
+ * Return: the count of set bits
+ */
+unsigned int count_set_bits(unsigned long int num)
+{
+	unsigned int count = 0;
+
+	while (num > 0)
+	{
+		count++;
+		n &= (n - 1);
+	}
+	return (count);
+}
+/**
+ * flip_bits - counts the number of bits to be flopped
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of flipped bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (count_set_bits(n ^ m));
+}
