1bce676f248684dc36b48fa2db17e9c490d23f1f
FREDRICK OKARA <okaraogunde@gmail.com>
2024-08-12 12:57:49 +0000

    2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * key_index - returns the index at which key/value pair is stored
+ * @key: the key
+ * @size: size of array of the hash table
+ *
+ * Return: the index 
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int value = hash_djb2(key);
+	unsigned long int index = value % size;
+
+	return (index);
+}
