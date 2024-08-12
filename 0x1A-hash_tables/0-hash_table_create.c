b483e01664c007717e88e1240cec3f460f91fa24
FREDRICK OKARA <okaraogunde@gmail.com>
2024-08-12 11:39:57 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	/* 
 	 * Creates a new Hash table
 	 */
-	long int i = 0;
+	unsigned long int i = 0;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return NULL;
