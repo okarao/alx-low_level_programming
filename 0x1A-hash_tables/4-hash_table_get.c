cbfa4d99bf670f84d89ae4fbbcb3f04f58ec88f8
FREDRICK OKARA <okaraogunde@gmail.com>
2024-08-14 10:54:55 +0000

    4-hash_table_get.c updates

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index(key);
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *item = ht->array[index];
 
 	if (ht == NULL || key == NULL || strlen(key) == 0)
