a94db15074a8cb61278dbc015ddc6ea6964162ed
FREDRICK OKARA <okaraogunde@gmail.com>
2024-08-14 07:23:23 +0000

    3-hash_table_set.c corrected

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,13 +13,13 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *new_node, *current_node;
+
 	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
 
-	unsigned long int index;
-	
 	index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *new_node, *current_node;
 	current_node = ht->array[index];
 
 	while (current_node != NULL)
