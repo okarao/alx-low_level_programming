f234a7023ca1f12482a2324de8c99b24421624e7
FREDRICK OKARA <okaraogunde@gmail.com>
2024-08-15 07:06:55 +0000

    6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * hash_table_delete - deletes a hash table
+ *
+ * @ht: the hash table
+ *
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current, *temp;
+
+	if (ht == NULL)
+		return;
+
+	if (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+
+		while (current != NULL)
+		{
+			temp =  current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+
+			current = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
